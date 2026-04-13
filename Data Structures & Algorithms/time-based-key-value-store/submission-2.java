7b548077ce827fc9eeb57036db47a2e09862a169
Andreas Santos <119018805+Andreas-Santos@users.noreply.github.com>
2026-04-12 21:22:42 -0300

    Add: time-based-key-value-store - submission-2

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-2.java
@@ -0,0 +1,36 @@
+class TimeMap {
+    Map<String, List<Pair<Integer, String>>> keystore;
+
+    public TimeMap() {
+        keystore = new HashMap<>();
+    }
+    
+    public void set(String key, String value, int timestamp) {
+        if(!keystore.containsKey(key)) {
+            keystore.put(key, new ArrayList<>());
+        }
+
+        keystore.get(key).add(new Pair<>(timestamp, value));
+    }
+    
+    public String get(String key, int timestamp) {
+        List<Pair<Integer, String>> list = keystore.getOrDefault(key, new ArrayList<>());
+        int left = 0;
+        int right = list.size() - 1;
+        String result = "";
+
+        while(left <= right) {
+            int middle = left + (right - left) / 2;
+
+            if(list.get(middle).getKey() <= timestamp) {
+                result = list.get(middle).getValue();
+                left = middle + 1;
+            }
+            else {
+                right = middle - 1;
+            }
+        }
+
+        return result;
+    }
+}
