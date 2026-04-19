4597f06a778f04944dcdebfd802d6b7eeacea3bf
Andreas Santos <119018805+Andreas-Santos@users.noreply.github.com>
2026-04-19 14:45:06 -0300

    Add: verifying-an-alien-dictionary - submission-1

---
+++ Data Structures & Algorithms/verifying-an-alien-dictionary/submission-1.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isAlienSorted(String[] words, String order) {
+        
+        Map<Character, Integer> mapLetters = new HashMap<>();
+
+        for(int i = 0; i < order.length(); i++) {
+            mapLetters.put(order.charAt(i), i);
+        }
+
+        int L = 0;
+        for(int R = 1; R < words.length; R++) {
+            int charIndex = 0;
+
+            while(charIndex < words[L].length() && charIndex < words[R].length() && words[L].charAt(charIndex) == words[R].charAt(charIndex)) {
+                charIndex++;
+            }
+
+            if(charIndex >= words[R].length()) return false;
+
+            if(charIndex >= words[L].length()) continue;
+
+            if(mapLetters.get(words[R].charAt(charIndex)) < mapLetters.get(words[L].charAt(charIndex))) {
+                return false;
+            }
+            
+            L++;
+        }
+
+
+        return true;
+    }
+}
