cba133c32b7d47693c73bdd2fbcb78558113ddd7
Andreas Santos <119018805+Andreas-Santos@users.noreply.github.com>
2026-04-28 23:23:43 -0300

    Add: buy-and-sell-crypto - submission-8

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-8.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+
+        int profit = 0;
+
+        int l = 0;
+        for(int r = 1; r < prices.length; r++) {
+
+            if(prices[l] < prices[r]) {
+                profit = Math.max(profit, prices[r] - prices[l]);
+    
+            }
+            else {
+                l = r;
+            }
+        }
+
+        return profit;
+    }
+}
